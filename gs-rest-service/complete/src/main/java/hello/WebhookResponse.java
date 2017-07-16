a979ee16320d187e979d43d4e830d36d7b1d953a
Chitra Sahai <Chitra.Sahai@cdk.com>
2017-07-17 01:27:53 +0530

    modified response object

--- gs-rest-service/complete/src/main/java/hello/WebhookResponse.java
+++ gs-rest-service/complete/src/main/java/hello/WebhookResponse.java
@@ -1,14 +1,23 @@
 package hello;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class WebhookResponse {
     private final String speech;
     private final String displayText;
 
     private final String source = "java-webhook";
-
+    private final List contextOut;
+    private final Object data;
+          
+    
     public WebhookResponse(String speech, String displayText) {
         this.speech = speech;
         this.displayText = displayText;
+        this.contextOut = new ArrayList();
+        this.data = new Object();
     }
 
     public String getSpeech() {
