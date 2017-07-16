853c6e148556ed53fa73b8434740ca6522931947
Chitra Sahai <Chitra.Sahai@cdk.com>
2017-07-17 00:50:21 +0530

    webhook

--- gs-rest-service/complete/src/main/java/hello/GreetingController.java
+++ gs-rest-service/complete/src/main/java/hello/GreetingController.java
@@ -1,8 +1,12 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,10 +20,13 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+   
     
-    @RequestMapping("/greeting1")
-    public Greeting greeting1(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    @RequestMapping(method = RequestMethod.POST)
+    public @ResponseBody WebhookResponse getModels(@RequestBody String obj){
+
+        System.out.println(obj);
+
+        return new WebhookResponse("Hello! " + obj, "Text " + obj);
     }
 }
